49c74db8b3a61c4da55d750fe3f4c37590cae662
jeikabu <jeikabu@users.noreply.github.com>
2021-02-10 18:54:50 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -46,3 +46,73 @@ pub mod tcpip {
         }
     }
 }
+
+pub mod freertos {
+    use crate::*;
+
+    #[repr(u8)]
+    enum QueueType {
+        /// `BASE`
+        Base				= 0,
+        //SET					= 0,
+        /// `MUTEX`
+        Mutex 				= 1,
+        /// `COUNTING_SEMAPHORE`
+        CountingSemaphore	= 2,
+        /// `BINARY_SEMAPHORE`
+        BinarySemaphore	= 3,
+        /// `RECURSIVE_MUTEX`
+        RecursiveMutex		= 4,
+    }
+
+    enum QueueCopyPosition {
+        /// `SEND_TO_BACK`
+        SendToBack		= 0,
+        /// `SEND_TO_FRONT`
+        SendToFront		= 1,
+        /// `OVERWRITE`
+        Overwrite			= 2,
+    }
+
+    /// portMAX_DELAY
+    pub const PORT_MAX_DELAY: TickType_t = !0;
+
+    pub fn xQueueCreate(ux_queue_length: UBaseType_t, ux_item_size: UBaseType_t) -> QueueHandle_t {
+        unsafe {
+            xQueueGenericCreate(ux_queue_length, ux_item_size, QueueType::Base as u8)
+        }
+    }
+
+    pub fn xQueueSendToBackFromISR(x_queue: QueueHandle_t, item_to_queue: *const core::ffi::c_void, higher_priority_task_woken: *mut BaseType_t) -> bool {
+        unsafe {
+            let retval = xQueueGenericSendFromISR(x_queue, item_to_queue, higher_priority_task_woken, QueueCopyPosition::SendToBack as BaseType_t);
+            retval != 0
+        }
+    }
+
+    pub fn xQueueReceive(x_queue: QueueHandle_t, buffer: *mut core::ffi::c_void, x_ticks_to_wait: TickType_t ) -> bool {
+        unsafe {
+            let retval = xQueueGenericReceive(x_queue, buffer, x_ticks_to_wait, false as BaseType_t);
+            retval != 0
+        }
+    }
+}
+
+pub mod driver {
+    // Sets the nth bit
+    const fn bit(n: u64) -> u64 {
+        0b01 << n
+    }
+
+    #[repr(u64)]
+    pub enum GpioSel {
+        Sel3  = bit(3),
+        Sel4  = bit(4),
+        Sel5  = bit(5),
+        Sel6  = bit(6),
+        Sel7  = bit(7),
+        Sel8  = bit(8),
+        Sel9  = bit(9),
+        Sel10 = bit(10),
+    }
+}
